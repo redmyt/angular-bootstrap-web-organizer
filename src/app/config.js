69b56eff13f5d2436e01374f0c75f9399b0c5c81
iriza7 <irynabautista@gmail.com>
2017-06-30 02:26:59 +0300

    Menu_for_routing

--- src/app/config.js
+++ src/app/config.js
@@ -1,14 +1,14 @@
 webOrganizerApp.config(function ($routeProvider) {
-    $routeProvider
+        $routeProvider
         .when('/', {
-            redirectTo: '/homePage'
+            templateUrl: './app/components/home/main.html'
         })
         .when('/homePage', {
             templateUrl: './app/components/home/homeView.html',
             controller: 'homeController',
             controllerAs: 'home'
         })
-        .when('/clock', {
+        .when('/#clock', {
             templateUrl: './app/components/home/digitalClock.html',
             controller: 'digitalClockController',
             controllerAs: 'dClock'
