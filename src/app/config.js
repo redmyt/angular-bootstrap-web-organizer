a14b56700f84355671b46fa5b3103071a4fa7a5e
dmytrorevak <dmytro27@gmail.com>
2017-07-04 15:18:21 +0300

    fix home routing redirect

--- src/app/config.js
+++ src/app/config.js
@@ -1,9 +1,6 @@
 webOrganizerApp.config(function($routeProvider) {
     $routeProvider
     .when('/', {
-        redirectTo: '/homePage'
-    })
-    .when('/homePage', {
         templateUrl : './app/components/home/homeView.html',
         controller: 'homeController',
         controllerAs: 'home'
