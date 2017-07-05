654193989d70df564a96c25bb9d616347bcd8e6e
dmytrorevak <dmytro27@gmail.com>
2017-07-05 14:15:25 +0300

    add function that cuts last entered char

--- src/app/components/calculator/calculatorController.js
+++ src/app/components/calculator/calculatorController.js
@@ -1,4 +1,11 @@
 webOrganizerApp.controller('calculatorController', function() {
     var vm = this;
-    vm.test = 'calc';
+
+
+    vm.removeLastChar = function () {
+        var lastEnterStringChar = vm.enterField.length - 1,
+            clippedEnterString = vm.enterField.slice(0, lastEnterStringChar);
+
+        vm.enterField = clippedEnterString;
+    };
 });
