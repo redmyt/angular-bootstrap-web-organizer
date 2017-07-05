7486908acca5d93959e1fb976736433b914a7277
dmytrorevak <dmytro27@gmail.com>
2017-07-05 14:32:32 +0300

    add functions which allow user to form the math sentence

--- src/app/components/calculator/calculatorController.js
+++ src/app/components/calculator/calculatorController.js
@@ -1,11 +1,25 @@
 webOrganizerApp.controller('calculatorController', function() {
+
     var vm = this;
 
+    vm.enterField = '';
 
-    vm.removeLastChar = function () {
+    vm.cleanEnterField = function() {
+        vm.enterField = '';
+    };
+
+    vm.removeLastChar = function() {
         var lastEnterStringChar = vm.enterField.length - 1,
             clippedEnterString = vm.enterField.slice(0, lastEnterStringChar);
 
         vm.enterField = clippedEnterString;
+    };
+
+    vm.enterNumericSymbol = function(numericSymbol) {
+        vm.enterField += numericSymbol;
+    };
+
+    vm.enterOperatorSymbol = function(operatorSymbol) {
+        vm.enterField += " " + operatorSymbol + " ";
     };
 });
